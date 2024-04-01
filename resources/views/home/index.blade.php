bc0a5e7ca1b33cf2cc45021eac76f51661a4938b
Fadlan123-prog <fadlan.diyanuar12@gmail.com>
2024-04-01 22:40:01 +0700

    add responsive size

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Habits&co</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('assets/images/Logo-no-bg.png')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
@@ -43,10 +44,10 @@
             <div class="row">
                 <div class="material__group">
 
-                    <div class="col-5">
+                    <div class="col-5 row__disable">
 
                     </div>
-                    <div class="col-7">
+                    <div class="col-md-7 col-lg-7 col-sm-12">
                         <div class="material__content">
                             <p>100% PREMIUM <span>COTTON COMBED 16S</span> AUSTRALIA COOLBREEZE</p>
 
@@ -67,7 +68,7 @@
                                 <div class="col">
                                     <div class="row row__height">
                                         <div class="col-5 centered__right">
-                                            <div class="">
+                                            <div class="image-">
                                                 <img class="" src="{{asset('assets/images/logo-shopee.png')}}" alt="Shopee">
                                             </div>
                                         </div>
@@ -115,11 +116,11 @@
             </div>
         </div>
     </section>
-    <section class="my-6">
+    <section class="my-6 gallery">
         <div class="container my-5 py-5">
             <div class="display__gallery">
                 <div class="row">
-                    <div class="col-7 d-flex flex-column justify-content-between">
+                    <div class="col-md-7 col-lg-7 col-sm-12 d-flex flex-column justify-content-between">
                         <div class="row w-70">
                             <span class="display__title">SEMUA MATA AKAN TERTUJU PADAMU.</span>
 
@@ -147,7 +148,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-5">
+                    <div class="col-5 row__disable">
                         <img src="{{asset('assets/images/Picture7.png')}}" alt="">
                     </div>
                 </div>
@@ -167,7 +168,7 @@
                 <div class="col centered__shop">
                     <p class="material__shop">SHOP NOW!</p>
 
-                    <p>Semua produk kami original dari Habits&Co selain dari kami itu palsu</p>
+                    <p class="material__desc">Semua produk kami original dari Habits&Co selain dari kami itu palsu</p>
                 </div>
                 <div class="col">
                     <div class="row row__height">
@@ -218,23 +219,23 @@
     </section>
     <section class="discount__price">
         <div class="container">
-            <div class="row">
+            <div class="row small__display">
                 <div class="col centered">
                     <h3><span class="today__discount">HANYA HARI INI </span><span class="discount__text">DISCOUNT!!!</span></h3>
                     <h3>Untuk 100 pembeli pertama</h3>
                 </div>
                 <div class="col">
                     <div class="row py-5">
-                        <h1>
+                        <span class="discount__through">
                             <span class="outer"><span class="inner">Rp. 802,500</span></span><span class="up__to"> up to</span><span class="percent__discount"> 64%</span>
-                        </h1>
-                        <h1 class="real__price">Rp. 288,900</h1>
+                        </span>
+                        <span class="real__price">Rp. 288,900</span>
                     </div>
                     <div class="row mt-8">
                         <div class="col centered__shop">
                             <p class="discount__shop">SHOP NOW!</p>
 
-                            <p>Semua produk kami original dari Habits&Co selain dari kami itu palsu</p>
+                            <p class="original__text">Semua produk kami original dari Habits&Co selain dari kami itu palsu</p>
                         </div>
                         <div class="col">
                             <div class="row row__height">
@@ -287,17 +288,17 @@
     </section>
     <section class="clothes__gallery">
         <div class="container py-5">
-            <div class="row">
-                <div class="col-3">
+            <div class="row gap__gallery">
+                <div class="col-6 col-lg-3">
                     <img src="{{asset('assets/images/clothes-1.png')}}" alt="">
                 </div>
-                <div class="col-3">
+                <div class="col-6 col-lg-3">
                     <img src="{{asset('assets/images/clothes-2.png')}}" alt="">
                 </div>
-                <div class="col-3">
+                <div class="col-6 col-lg-3">
                     <img src="{{asset('assets/images/clothes-3.png')}}" alt="">
                 </div>
-                <div class="col-3">
+                <div class="col-6 col-lg-3">
                     <img src="{{asset('assets/images/clothes-4.png')}}" alt="">
                 </div>
             </div>
@@ -307,24 +308,24 @@
     <section class="description__sec">
         <div class="container">
             <div class="py-5 my-6">
-                <div class="row">
-                    <div class="col-5">
+                <div class="row small__display">
+                    <div class="col-12 col-lg-5">
                         <img class="model__pic" src="{{asset('assets/images/model-desc.png')}}" alt="">
                     </div>
-                    <div class="col-7 desc__page">
+                    <div class="col-12 col-lg-7 desc__page">
                         <div class="row">
                             <h2 class="desc__title">
                                 KAOS OVERSIZE MEWAH 16S DENGAN HARGA GROSIR, SETARA BRAND EXCLUSIVE
                             </h2>
-                            <p class="mt-3">
+                            <p class="mt-3 small__font">
                                 Tunggu apalagi, tambahkan koleksi outfit anda menjadi lebih bergaya. BUKAN kaos abal abal dengan kualitas murahan.
                             </p>
                         </div>
-                        <div class="row">
+                        <div class="row mt-8">
                             <div class="col centered__shop">
-                                <p class="discount__sh`op">SHOP NOW!</p>
+                                <p class="discount__shop">SHOP NOW!</p>
 
-                                <p>Semua produk kami original dari Habits&Co selain dari kami itu palsu</p>
+                                <p class="font__8p">Semua produk kami original dari Habits&Co selain dari kami itu palsu</p>
                             </div>
                             <div class="col">
                                 <div class="row row__height">
@@ -378,11 +379,11 @@
     </section>
     <section class="footer py-5">
         <div class="container py-5">
-            <div class="row">
-                <div class="col-6 tagline__footer">
-                    <h2>Masih bingung nentuin <span>sizenya</span> ?</h2>
+            <div class="row gap__contact">
+                <div class="col-12 col-lg-6 tagline__footer">
+                    <h2 class="font-24">Masih bingung nentuin <span>sizenya</span> ?</h2>
                 </div>
-                <div class="col-3 gap__footer">
+                <div class="col-lg-3 col-6 gap__footer">
                     <div class="row">
                         <div class="col call__me">
                              Hubungi kami Yuk
@@ -390,23 +391,23 @@
 
                     </div>
                     <div class="row mt-3">
-                        <div class="col-2">
-                            <img src="{{asset('assets/images/logo-whatsapp.png')}}" alt="">
+                        <div class="col-lg-2 col-3">
+                            <img class="social__logo" src="{{asset('assets/images/logo-whatsapp.png')}}" alt="">
                         </div>
-                        <div class="col-10">
+                        <div class="col-lg-10 col-9 align-center">
                             <p class="social__media">0823-2121-7772</p>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-2">
-                            <img src="{{asset('assets/images/logo-instagram.png')}}" alt="">
+                        <div class="col-lg-2 col-3">
+                            <img class="social__logo" src="{{asset('assets/images/logo-instagram.png')}}" alt="">
                         </div>
-                        <div class="col-10">
+                        <div class="col-lg-10 col-9 align-center">
                             <p class="social__media">@habitsandco.official</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-3 gap__footer">
+                <div class="col-lg-3 col-6 gap__footer">
                     <div class="row">
                         <div class="col call__me">
                              Atau hubungi admin kami
@@ -415,10 +416,10 @@
                     </div>
                     <div class="row ">
                         <div class="col-3 m-auto">
-                            <img src="{{asset('assets/images/logo-shopee.png')}}" alt="">
+                            <img class="ecommerce__footer" src="{{asset('assets/images/logo-shopee.png')}}" alt="">
                         </div>
                         <div class="col-9">
-                            <img src="{{asset('assets/images/logo-tokped.png')}}" alt="">
+                            <img class="ecommerce__footer" src="{{asset('assets/images/logo-tokped.png')}}" alt="">
                         </div>
                     </div>
                 </div>
@@ -428,20 +429,20 @@
     <section>
         <div class="container">
             <div class="promotion">
-                <h1>
+                <h1 class="font-26">
                     HANYA UNTUK <span>100 PEMBELI</span> PERTAMA YAH !!
                 </h1>
-                <h1>HAPPY SHOPPING !!</h1>
+                <h1 class="font-26">HAPPY SHOPPING !!</h1>
             </div>
         </div>
     </section>
     <section class="sec__footer">
         <div class="container-fluid py-5">
             <div class="row justify-content-between align-items-center">
-                <div class="col-4">
-                  <h1>Contact Us</h1>
+                <div class="col-4 col-md-4">
+                  <h1 class="font-26">Contact Us</h1>
                 </div>
-                <div class="col-4">
+                <div class="col-lg-8 col-md-8 col-8">
                     <div class="row">
                         <div class="col-6 d-flex">
                           <img src="{{asset('assets/images/white-instagram.png')}}" alt="">
